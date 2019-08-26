13f0418169467482ba82cb6052783a0e89771340
Tom French <tomfrench100@gmail.com>
2019-08-26 21:59:30 +0100

    Return 180 when 4 A's test.

--- CheckoutKata.Test/CheckoutTests.cs
+++ CheckoutKata.Test/CheckoutTests.cs
@@ -55,5 +55,20 @@ namespace CheckoutKata.Test
             
             Assert.AreEqual(130, totalPrice);
         }
+        
+        [Test]
+        public void WhenFourProductAsAreScanned_GetTotalPriceReturnsOfferPriceSummedWithIndividualPrice()
+        {
+            ICheckout checkout = new Checkout(new FileLoader());
+            var totalPrice = 0;
+            
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            totalPrice = checkout.GetTotalPrice();
+            
+            Assert.AreEqual(180, totalPrice);
+        }
     }
 }
